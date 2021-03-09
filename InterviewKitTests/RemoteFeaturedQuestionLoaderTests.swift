c15a9b8287ab6a6944503c7b4bb65427d6d89c1c
Alexander Nikolaychuk <alexksnikol@gmail.com>
2021-03-09 11:10:07 +0200

    Move test logic  to a separate type `HTTPClientSpy`

--- InterviewKitTests/RemoteFeaturedQuestionLoaderTests.swift
+++ InterviewKitTests/RemoteFeaturedQuestionLoaderTests.swift
@@ -8,20 +8,45 @@
 import XCTest
 
 class RemoteFeaturedLoader {
-    
+    func load() {
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+    }
 }
 
 class HTTPClient {
+    
+    static var shared = HTTPClient()
+    
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
+    
     var requestedURL: URL?
+    
+    override func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class RemoteFeaturedQuestionLoaderTests: XCTestCase {
     
     func test_init_doesntNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeaturedLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_init_requetsDataFromURL() {
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
+        let sut = RemoteFeaturedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
     
 }
