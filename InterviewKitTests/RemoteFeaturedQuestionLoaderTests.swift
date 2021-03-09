2aa3500f4c4c730d657e4f04c91119083907dc85
Alexander Nikolaychuk <alexksnikol@gmail.com>
2021-03-09 11:19:33 +0200

    Given a URL instead of hardcoding in the `RemoteFeaturedLoader`

--- InterviewKitTests/RemoteFeaturedQuestionLoaderTests.swift
+++ InterviewKitTests/RemoteFeaturedQuestionLoaderTests.swift
@@ -9,13 +9,15 @@ import XCTest
 
 class RemoteFeaturedLoader {
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -36,18 +38,19 @@ class RemoteFeaturedQuestionLoaderTests: XCTestCase {
     
     func test_init_doesntNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        _ = RemoteFeaturedLoader(client: client)
+        _ = RemoteFeaturedLoader(url: URL(string: "https://a-url.com")!, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_init_requetsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeaturedLoader(client: client)
+        let sut = RemoteFeaturedLoader(url: url, client: client)
         
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
     
 }
